41a4d0e5068f48a5e2aae9794e10dce1379e7879
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-02-19 08:34:05 +0600

    Add files via upload

---
+++ incourse_solve/Main1.java
@@ -0,0 +1,56 @@
+//Explain the concept of polymorphism using examples
+
+//Polymorphism- is the ability to take on different form
+//method overloading-compile time
+//method overiding -run time 
+
+//overloading - allows different methods to have same name but different paraeters
+
+class Demo //method overloading
+{
+    void test()
+    {
+        System.out.println("No parameter");
+    }
+    void test(int a)
+    {
+        System.out.println("a : "+a);
+    }
+    void test(double a, double b)
+    {
+        System.out.println("a : "+a +" b :"+b);
+    }
+}
+
+//method overriding : is a feature where a child class provides a new implementation for a 
+//method that is already defined in its parent class, while keeping same method name and parameter
+
+class Animal
+{
+    void sound()
+    {
+        System.out.println("Animals are barking");
+    }
+}
+class Dog extends Animal
+{
+    void hello()
+    {
+         System.out.println("Hello from dog");
+    }
+}
+class Main1
+{
+    public static void main(String[] args)
+    {
+        Demo a=new Demo();
+        a.test();
+        a.test(6);
+        a.test(78.9,90.5);
+        Animal p=new Animal();
+        p.sound();
+        Dog x=new Dog();
+        x.hello();
+        x.sound();
+    }
+} 
