4c61f7e47533354c7c3ea698f3d88e22986beacc
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-02-20 09:47:56 +0600

    Add files via upload

---
+++ incourse_solve/Main3.java
@@ -0,0 +1,23 @@
+/* Find the isuues in this programme */
+
+public interface A    
+{
+    public void printA();  //printA() method need to be implemented
+}
+class Simple extends A{ // it should  be implements. Class does not extend interface it implements  
+
+    int x=10;
+    static void access()
+    {
+        System.out.println("x "+x); // static method can only access static variable. Here x is defined as int not static
+    }
+
+abstract void simplePrint(); //abstract method can only be defined in abstract class
+}
+public class CodeProblem
+{
+    public void main(String[] args) // public static void main
+    {
+        Simple s=new Simple();
+    }
+}
